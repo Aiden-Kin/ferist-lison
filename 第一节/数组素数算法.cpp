f7324815e003b279e4c5e201d4a4881b7cc0fb33
23166 <2316664125@qq.com>
2022-10-20 20:26:01 +0800

    更改素数算法

--- 第一节/数组素数算法.cpp
+++ 第一节/数组素数算法.cpp
@@ -3,24 +3,64 @@
 
 int main()
 {
-	int a[]={1,2,3,4,5,6,7,8,9}, b[]={0};
+	int a[]={1,2,3,4,5,6,7,8,9}, b[10]={0},bcont;
 
-	void prime(int a[],int b[]);
+	int prime(int a[],int b[]);
 
-	prime(a, b);
+	bcont = prime(a, b);					//bcont为素数个数  a 传入数据集合， b 传出素数数据集
 
-	for (int k = 0; k < 10; k++)
+	for (int k = 0; k < bcont; k++)
 
 		printf("%4d", b[k]);
 
 	return 0;
 }
 
-void prime(int a[], int b[])		// 素数函数，a[]为传入数列 b[]为传出素数数列
-{
-	int i,j,bcont=0,m;
+//int prime(int a[], int b[])		// 素数函数，a[]为传入数列 b[]为传出素数数列
+//{
+//	int i,j,bcont=0,m;
+//
+//	for (i=0; i < 10; i++)			//10 为a[]数组的个数
+//	{
+//		if (a[i] == 1 || a[i] == 3)
+//		{
+//			b[bcont] = a[i];
+//
+//			bcont++;
+//
+//			continue;
+//		}
+//
+//		m = 0;
+//
+//		if (a[i] % 2 == 0)
+//		{
+//			m=1;
+//		}
+//		for(j=3;j<10;j+=2)
+//			if (a[i] % j == 0)
+//			{
+//				m=1;
+//			}
+//		
+//		if (m == 1) continue;
+//
+//		b[bcont] = a[i];
+//
+//		bcont++;
+//
+//		
+//
+//	}
+//
+//
+//}
 
-	for (i=0; i < 10; i++)			//10 为a[]数组的个数
+int prime(int a[], int b[])		// 素数函数，a[]为传入数列 b[]为传出素数数列
+{
+	int i, j, bcont = 0, m;
+
+	for (i = 0; i < 10; i++)			//10 为a[]数组的个数
 	{
 		if (a[i] == 1 || a[i] == 3)
 		{
@@ -35,23 +75,24 @@ void prime(int a[], int b[])		// 素数函数，a[]为传入数列 b[]为传出素数数列
 
 		if (a[i] % 2 == 0)
 		{
-			m=1;
+			m = 1;
 		}
-		for(j=3;j<10;j+=2)
+		for (j = 3; j < a[i]; j += 2)
 			if (a[i] % j == 0)
 			{
-				m=1;
+				m = 1;
 			}
-		
+
 		if (m == 1) continue;
 
 		b[bcont] = a[i];
 
 		bcont++;
 
-		
+
 
 	}
 
+	return bcont;
 
 }
