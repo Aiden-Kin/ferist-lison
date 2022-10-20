6528642b5b3cb3a596e178515ffb36b8beb80c25
23166 <2316664125@qq.com>
2022-10-20 22:50:31 +0800

    修改算法

--- 第一节/数组素数算法.cpp
+++ 第一节/数组素数算法.cpp
@@ -16,45 +16,6 @@ int main()
 	return 0;
 }
 
-//int prime(int a[], int b[])		// 素数函数，a[]为传入数列 b[]为传出素数数列
-//{
-//	int i,j,bcont=0,m;
-//
-//	for (i=0; i < 10; i++)			//10 为a[]数组的个数
-//	{
-//		if (a[i] == 1 || a[i] == 3)
-//		{
-//			b[bcont] = a[i];
-//
-//			bcont++;
-//
-//			continue;
-//		}
-//
-//		m = 0;
-//
-//		if (a[i] % 2 == 0)
-//		{
-//			m=1;
-//		}
-//		for(j=3;j<10;j+=2)
-//			if (a[i] % j == 0)
-//			{
-//				m=1;
-//			}
-//		
-//		if (m == 1) continue;
-//
-//		b[bcont] = a[i];
-//
-//		bcont++;
-//
-//		
-//
-//	}
-//
-//
-//}
 
 int prime(int a[], int b[])		// 素数函数，a[]为传入数列 b[]为传出素数数列
 {
--- 第一节/测试.c
+++ 第一节/测试.c
@@ -1,50 +1,33 @@
-﻿#include<stdio.h>
+﻿#include <stdio.h>
+#include <math.h>
 
-#include<math.h>
+int main()
+{
+    int a[10]={1,2,3,4,5,6,7,8,9,10}, i, j, n = 0, sum = 0, b[10];
 
-int main() {
-    int n = 0, a[20];
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &a[i]);
-        int isP = 1;
-        if (a[i] >= 4) {
-            for (int j = 2; j <= sqrt(a[i]); j++) {
-                if (a[i] % j == 0) {
-                    isP = 0;
-                    break;
-                }
-            }
-        }
-        else if (a[i] == 1)
-            isP = 0;
-        if (isP) {
-            a[n] = a[i];
+    float ave;
+
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 2; j < a[i]; j++)
+
+            if (a[i] % j == 0)break;
+
+        if (j == a[i])
+        {
+            sum += a[i];
             n++;
+            printf("%d\n", a[i]);
         }
     }
 
-    for (int i = 0; i < n - 1; i++) {
+   
 
-        for (int j = i + 1; j < n; j++) {
 
-            if (a[i] > a[j]) {
+    ave = (float)sum / n;
 
-                int temp = a[i];
-
-                a[i] = a[j];
-
-                a[j] = temp;
-
-            }
-
-        }
-
-    }
-
-    for (int i = 0; i < n; i++)
-
-        printf("%5d", a[i]);
+    printf("%.2f\n", ave);
 
     return 0;
-
 }
